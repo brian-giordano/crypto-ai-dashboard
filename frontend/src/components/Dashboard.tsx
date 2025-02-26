6531fa69a078f214f3fcafd0743db7868c93b3e4
Brian Giordano <bgiordano84@gmail.com>
2025-02-26 15:54:42 -0500

    Update Dashboard to take 2 colums of cards on mobile.

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -21,7 +21,7 @@ const Dashboard: React.FC = () => {
         </div>
       ) : (
         // Use a grid layout for displaying DashboardCard components
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3">
           {dashboardCryptos.map((item) => (
             <DashboardCard
               key={item.id} // Unique key for each card
